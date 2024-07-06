81e070ffe5820cc00fd2a1d6f45399f81801b55f
Sameer413 <92072016+Sameer413@users.noreply.github.com>
2024-07-06 18:21:24 +0530

    changes

--- server.ts
+++ server.ts
@@ -9,37 +9,28 @@ dotenv.config();
 
 app.use(express.json());
 
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   process.env.FRONTEND_URL || "",
-// ];
-
-// const corsOptions = {
-//   origin: function (
-//     origin: string | undefined,
-//     callback: (err: Error | null, allow?: boolean) => void
-//   ) {
-//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true,
-// };
+const allowedOrigins = [
+  "http://localhost:3000",
+  process.env.FRONTEND_URL || "",
+];
 
 const corsOptions = {
-  origin:[
-    "http://localhost:3000",
-    "https://localhost:3000",
-    "http://localhost:3000/"
-  ]
-}
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 app.route("/").get((req: Request, res: Response) => {
   return res.status(200).json({
